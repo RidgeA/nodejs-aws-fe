e19bf74172141329b145d8cc447b11b9e870ecae
RidgeA <antongriadchenko@gmail.com>
2020-10-31 00:25:42 +0200

    feat: add product service integration

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://2to564lyf9.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'https://2to564lyf9.execute-api.eu-west-1.amazonaws.com/dev',
 };
 
 export default API_PATHS;
